012e591fa221460247fbffc1db1cbcf7d2ac2458
Cold Head <certainthings@gmail.com>
2015-01-13 09:30:42 +1100

    Ruby intro

---
+++ 01-ruby/basics.rb
@@ -0,0 +1,49 @@
+# Here are some basic Ruby examples.
+puts "Hello world from Ruby"
+
+puts 5 * 13
+
+# TYPES
+
+# Integers
+1
+5
+-1001
+
+# Decimals or floating points
+0.67
+157.2
+-0.45
+
+# Strings
+"this has double quotes"
+'this has single quotes'
+"this looks a bit \"iffy\" to me"
+"i can't even"
+
+# Booleans
+true
+false
+
+
+# VARIABLES
+snake_case = 'underscore_separated' # Assignment
+current_speed = 1000
+# SomeClassNameUsingCamelCase
+ANSWER_TO_EVERYTHING = 42 # This is a constant
+
+2 * 3
+1 - 5
+4 / 2
+5 / 5
+5 % 2 # Modulus operator.
+
+5 ** 5 # Power operator.
+
+
+
+
+
+
+
+
---
+++ 01-ruby/strings.rb
@@ -0,0 +1,11 @@
+puts "What is your first name?"
+first_name = gets.chomp
+
+puts "What is your surname?"
+last_name = gets.chomp
+
+puts "Your full name is #{first_name} #{last_name}"
+
+puts "What is your suburb?"
+suburb = gets.chomp
+puts "You live in #{ suburb }"
