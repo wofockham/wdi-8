49c528bf52caf23d12a5102aaf38ed8064137ff6
Cold Head <certainthings@gmail.com>
2015-01-13 16:19:43 +1100

    legal age function

--- 01-ruby/legal_age.rb
+++ 01-ruby/legal_age.rb
@@ -1,8 +1,12 @@
 print "What is your age? "
 age = gets.chomp.to_i # This will be 0 if the user enters non-numeric text.
 
-if age < 18
-  puts "An appropriate message"
-else
-  puts "A different message"
+def can_drink(current_age)
+  if current_age < 18
+    puts "No drinks for you"
+  else
+    puts "Have a drink"
+  end
 end
+
+can_drink(age)
