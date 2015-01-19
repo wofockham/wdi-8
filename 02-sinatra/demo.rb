5ad8a70caa983075ac0e2779d0fc6d4a2ef719ec
Cold Head <certainthings@gmail.com>
2015-01-19 12:53:47 +1100

    broken calc

--- 02-sinatra/demo.rb
+++ 02-sinatra/demo.rb
@@ -1,6 +1,34 @@
 require 'sinatra'
+require 'sinatra/reloader'
 
 get '/hello' do
   "Hello world from Sinatra"
 end
+
+get '/help' do
+  "Hello World"
+end
+
+get '/joel' do
+  "Hello Joel, have a nice day (you stupid jerk)"
+end
+
+get '/greeting/:name' do
+  "Hello " + params[:name].swapcase
+end
+
+get '/greeting/:first/:last' do
+  "Hello there Mr or Ms #{ params[:first] } #{ params[:last].upcase }"
+end
+
+get '/greeting/:first/:last/:age' do
+  "#{ params[:first] } is #{ params[:age] } years old"
+end
+
+get '/multiply/:x/:y' do
+  result = params[:x] * params[:y]
+  "The result is #{ result }"
+end
+
+
 
