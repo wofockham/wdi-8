be84e1574b0e3bd218907d772f5444778f74e2eb
Cold Head <certainthings@gmail.com>
2015-01-19 17:02:29 +1100

    stock demo

--- 02-sinatra/stocklab/main.rb
+++ 02-sinatra/stocklab/main.rb
@@ -1,6 +1,15 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'yahoofinance'
 
 get '/' do
+  erb :lookup
+end
+
+get '/lookup' do
+  @symbol = params[:stock_symbol].upcase
+  result = YahooFinance::get_quotes(YahooFinance::StandardQuote, @symbol)
+  @price = result[@symbol].lastTrade if result[@symbol]
+
   erb :lookup
 end
