a70ae4158c4de233f73bf3f540448f19d01cb5f5
Cold Head <certainthings@gmail.com>
2015-01-19 15:28:04 +1100

    thanks

--- 02-sinatra/calc/main.rb
+++ 02-sinatra/calc/main.rb
@@ -10,3 +10,17 @@ get '/add/:x/:y' do
   @result = params[:x].to_f + params[:y].to_f
   erb :calc
 end
+
+get '/calc' do
+  @first = params[:first].to_f
+  @second = params[:second].to_f
+
+  @result = case params[:operator]
+  when '+' then @first + @second
+  when '-' then @first - @second
+  when '*' then @first * @second
+  when '/' then @first / @second
+  end
+
+  erb :calc
+end
