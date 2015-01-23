e9146b1c4089c74a07336089797e9f069de245ca
Cold Head <certainthings@gmail.com>
2015-01-23 13:01:00 +1100

    all receipts skellington

--- 02-sinatra/receipts/main.rb
+++ 02-sinatra/receipts/main.rb
@@ -5,3 +5,22 @@ require 'pry'
 get '/' do
   erb :form
 end
+
+post '/' do
+  db = File.open('receipts.csv', 'a+')
+  db.puts params.values.join(',')
+  db.close
+  erb :thanks
+end
+
+get '/all' do
+  db = File.open('receipts.csv', 'r')
+  @receipts = []
+
+  db.each do |line|
+    @receipts << line.split(',')
+  end
+
+  db.close
+  erb :all
+end
