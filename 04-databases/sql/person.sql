6196d35949cfeee1f6fc66b988a727594c0a0305
Cold Head <certainthings@gmail.com>
2015-01-27 17:14:03 +1100

    db examples

--- 04-databases/sql/person.sql
+++ 04-databases/sql/person.sql
@@ -17,3 +17,32 @@ CREATE TABLE person_pet (
   person_id INTEGER,
   pet_id INTEGER
 );
+
+INSERT INTO person (id, first_name, last_name, age) VALUES (0, "Zed", "Shaw", 37);
+INSERT INTO pet (id, name, breed, age, dead) VALUES (0, "Fluffy", "jack", 1000, 0);
+INSERT INTO pet VALUES (1, 'Gigantor', 'Robot', 1, 1);
+
+
+INSERT INTO person_pet (person_id, pet_id) VALUES (0, 0);
+INSERT INTO person_pet VALUES (0, 1);
+
+DELETE FROM pet WHERE dead = 1;
+
+-- Update example
+UPDATE person SET first_name = 'Hilarous Guy' WHERE first_name = 'Zed';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
