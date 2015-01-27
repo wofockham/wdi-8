c14795595406a4beff1ebf604c3b030e3282636a
Cold Head <certainthings@gmail.com>
2015-01-27 14:35:22 +1100

    index and show

--- 04-databases/butterflies/main.rb
+++ 04-databases/butterflies/main.rb
@@ -8,3 +8,23 @@ get '/' do
   erb :home
 end
 
+get '/butterflies' do
+  db = SQLite3::Database.new "butterflies.db"
+  db.results_as_hash = true
+
+  @butterflies = db.execute "SELECT * FROM butterflies"
+
+  erb :index
+end
+
+get '/butterflies/:id' do
+  db = SQLite3::Database.new "butterflies.db"
+  db.results_as_hash = true
+
+  id = params[:id]
+
+  @butterfly = db.execute "SELECT * FROM butterflies WHERE id = #{ id }"
+  @butterfly = @butterfly.first
+
+  erb :show
+end
