dacd90aee40b27ab8f04bce05d6e60b5223e522d
Cold Head <certainthings@gmail.com>
2015-01-27 14:54:40 +1100

    new butterfly form

--- 04-databases/butterflies/main.rb
+++ 04-databases/butterflies/main.rb
@@ -9,22 +9,38 @@ get '/' do
 end
 
 get '/butterflies' do
-  db = SQLite3::Database.new "butterflies.db"
-  db.results_as_hash = true
-
-  @butterflies = db.execute "SELECT * FROM butterflies"
-
+  @butterflies = db_query("SELECT * FROM butterflies")
   erb :index
 end
 
-get '/butterflies/:id' do
-  db = SQLite3::Database.new "butterflies.db"
-  db.results_as_hash = true
+get '/butterflies/new' do
+  erb :new
+end
 
+post '/butterflies' do
+  "This seems to be working"
+end
+
+get '/butterflies/:id' do
   id = params[:id]
 
-  @butterfly = db.execute "SELECT * FROM butterflies WHERE id = #{ id }"
+  @butterfly = db_query("SELECT * FROM butterflies WHERE id = #{ id }")
   @butterfly = @butterfly.first
 
   erb :show
 end
+
+
+
+
+
+def db_query(sql)
+  db = SQLite3::Database.new "butterflies.db"
+  db.results_as_hash = true
+
+  result = db.execute sql
+
+  db.close
+  result
+end
+
