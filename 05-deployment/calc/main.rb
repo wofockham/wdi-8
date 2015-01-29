fd7fcc8fc06e2ec63f7ea037ebcf7476908961ed
Cold Head <certainthings@gmail.com>
2015-01-30 10:34:38 +1100

    Gemfile and config.ru for deployment

---
+++ 05-deployment/calc/main.rb
@@ -0,0 +1,25 @@
+require 'sinatra'
+
+get '/multiply/:x/:y' do
+  @result = params[:x].to_f * params[:y].to_f
+  erb :calc
+end
+
+get '/add/:x/:y' do
+  @result = params[:x].to_f + params[:y].to_f
+  erb :calc
+end
+
+get '/calc' do
+  @first = params[:first].to_f
+  @second = params[:second].to_f
+
+  @result = case params[:operator]
+  when '+' then @first + @second
+  when '-' then @first - @second
+  when '*' then @first * @second
+  when '/' then @first / @second
+  end
+
+  erb :calc
+end
