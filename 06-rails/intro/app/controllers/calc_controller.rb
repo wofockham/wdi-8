effb6db226e4cc40c1123a1aaa22a0d95015bde0
Cold Head <certainthings@gmail.com>
2015-02-02 14:13:02 +1100

    calculating

--- 06-rails/intro/app/controllers/calc_controller.rb
+++ 06-rails/intro/app/controllers/calc_controller.rb
@@ -1,5 +1,14 @@
 class CalcController < ApplicationController
   def calculate
+    @x = params[:x].to_f
+    @y = params[:y].to_f
 
+    @result = case params[:operator]
+    when '+' then @x + @y
+    when '-' then @x - @y
+    when '*' then @x * @y
+    when 'div' then @x / @y
+    when '**' then @x ** @y
+    end
   end
 end
