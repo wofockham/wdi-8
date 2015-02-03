af631a5e76ab45434e97422e9c68da4e955cc125
Cold Head <certainthings@gmail.com>
2015-02-04 10:47:45 +1100

    mikael is homebrew genius

---
+++ 06-rails/moma/app/models/artist.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: artists
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  nationality :string
+#  dob         :date
+#  period      :string
+#  image       :text
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
+class Artist < ActiveRecord::Base
+end
---
+++ 06-rails/moma/app/models/work.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: works
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  year       :string
+#  medium     :string
+#  style      :string
+#  image      :text
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class Work < ActiveRecord::Base
+end
