342b30863d40d4ea9232cab3ec1865357b1d6767
Cold Head <certainthings@gmail.com>
2015-02-03 12:59:52 +1100

    planets

--- 06-rails/solar_system/app/models/planet.rb
+++ 06-rails/solar_system/app/models/planet.rb
@@ -2,14 +2,16 @@
 #
 # Table name: planets
 #
-#  id       :integer          primary key
-#  name     :text
-#  image    :text
-#  orbit    :float
-#  diameter :float
-#  distance :float
-#  mass     :float
-#  moons    :integer
+#  id         :integer          not null, primary key
+#  name       :string
+#  image      :text
+#  orbit      :float
+#  mass       :float
+#  diameter   :float
+#  distance   :float
+#  moons      :integer
+#  created_at :datetime
+#  updated_at :datetime
 #
 
 class Planet < ActiveRecord::Base
