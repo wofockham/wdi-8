f7f4747bae9ffe3a34ae09598f09b16a3bcd11ad
Cold Head <certainthings@gmail.com>
2015-02-05 11:48:51 +1100

    harpies

--- 06-rails/tunr/db/seeds.rb
+++ 06-rails/tunr/db/seeds.rb
@@ -32,24 +32,32 @@ m3 = Mixtape.create(:name => 'Stupid Everything')
 # Associate songs and artists:
 r1.songs << s1
 r3.songs = [s2]
+r3.save
 s3.artist = r2
+s3.save
 
 # Associate songs and genres:
 g1.songs << s1
 g2.songs = [s2]
+g2.save
 s3.genres << g3
 
 # Associates songs and albums:
 s1.album = a1
+s1.save
 s2.album = a2
+s2.save
 a3.songs << s3
 
 # Associate songs and mixtapes:
 m1.songs = [s1, s2, s3]
+m1.save
 m2.songs << s1 << s2
 m3.songs << s3
 
 # Associate mixtapes and users:
 u1.mixtapes << m1
 m2.user = u2
+m2.save
 m3.user = u3
+m3.save
