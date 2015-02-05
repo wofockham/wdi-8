d8d1188caed217dfbc120d1bf82819b01084aa06
Cold Head <certainthings@gmail.com>
2015-02-05 11:31:01 +1100

    music seed

--- 06-rails/tunr/db/seeds.rb
+++ 06-rails/tunr/db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Genre.destroy_all
+Song.destroy_all
+Album.destroy_all
+Artist.destroy_all
+User.destroy_all
+Mixtape.destroy_all
+
+g1 = Genre.create(:name => 'Nautical Text Rock')
+g2 = Genre.create(:name => 'Post jazz')
+g3 = Genre.create(:name => 'Ancient')
+
+s1 = Song.create(:name => 'Good Morning Captain')
+s2 = Song.create(:name => 'Tin Cans and Twine')
+s3 = Song.create(:name => 'Fratres')
+
+a1 = Album.create(:name => 'Spiderland')
+a2 = Album.create(:name => 'Tortoise')
+a3 = Album.create(:name => 'Spiegel')
+
+r1 = Artist.create(:name => 'Slint')
+r2 = Artist.create(:name => 'Arvo Part')
+r3 = Artist.create(:name => 'Tortoise')
+
+u1 = User.create(:name => 'Marty')
+u2 = User.create(:name => 'Sam')
+u3 = User.create(:name => 'Miriam')
+
+m1 = Mixtape.create(:name => 'Code jams')
+m2 = Mixtape.create(:name => 'Morning Music')
+m3 = Mixtape.create(:name => 'Stupid Everything')
+
