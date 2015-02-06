812b0e1c88341131cffb522c3b7ca7399e291961
Cold Head <certainthings@gmail.com>
2015-02-06 11:24:21 +1100

    hotdogs

--- 06-rails/tunr/app/helpers/application_helper.rb
+++ 06-rails/tunr/app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def nav_menu
+    links = "<li>#{ link_to('Home', root_path) }</li>"
+    if @current_user.present?
+      links += "<li>#{ link_to('Sign out ' + @current_user.name, login_path, :method => :delete) }</li>"
+    else
+      links += "<li>#{ link_to('Sign up', new_user_path) }</li><li>#{ link_to('Log in', login_path) }</li>"
+    end
+
+    links
+  end
 end
