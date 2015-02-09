e153250b374c660549946d346b4058c9404a6c91
Cold Head <certainthings@gmail.com>
2015-02-09 15:09:43 +1100

    columns

--- 06-rails/tunr/config/routes.rb
+++ 06-rails/tunr/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root :to => 'pages#home'
+
+  get '/about' => 'pages#about'
+
   resources :users, :only => [:new, :create, :index]
 
   get '/login' => 'session#new'
