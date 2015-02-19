376ef9ba4cd2a5fd694e3105dd4c5e3a3a81dbb6
Cold Head <certainthings@gmail.com>
2015-02-19 16:12:19 +1100

    shorter

--- 07-javascript/videowatcher/videowatcher.js
+++ 07-javascript/videowatcher/videowatcher.js
@@ -2,5 +2,5 @@ var $links = $('a');
 
 $links.each(function (i, elem) {
   var thumb = youtube.generateThumbnailUrl(elem.href);
-  $('<img src="' + thumb + '">').appendTo(elem);
+  $('<img>', {src: thumb}).appendTo(elem);
 });
