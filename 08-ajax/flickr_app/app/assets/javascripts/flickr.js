018b42f2e1de677bc5a906f13a07bd4cd741b955
Cold Head <certainthings@gmail.com>
2015-02-24 12:48:10 +1100

    endOfPage

--- 08-ajax/flickr_app/app/assets/javascripts/flickr.js
+++ 08-ajax/flickr_app/app/assets/javascripts/flickr.js
@@ -1,5 +1,11 @@
 var currentPage = 1;
+var endOfImages = false;
+
 var searchFlickr = function () {
+  if (endOfImages === true) {
+    return; // Abort!
+  }
+
   var query = $('#query').val();
 
   // All requests to the Flickr REST API are based off this URL.
@@ -11,7 +17,7 @@ var searchFlickr = function () {
     api_key: '2f5ac274ecfac5a455f38745704ad084',
     text: query,
     format: 'json',
-    per_page: 100,
+    per_page: 500,
     page: currentPage++
   }).done(processImages); // Our processImages callback displays the images on the page.
 };
@@ -19,7 +25,12 @@ var searchFlickr = function () {
 var throttledSearchFlickr = _.throttle(searchFlickr, 5000);
 
 var processImages = function(result) {
+
   var photos = result.photos.photo; // Extract just the part of the data we care about.
+
+  if (currentPage > result.photos.pages) {
+    endOfImages = true;
+  }
 
   _(photos).each(function (photo) {
     // Construct an image URL from the data Flickr returns for each photo:
@@ -60,12 +71,13 @@ $(document).ready(function () {
     searchFlickr();
   });
 
-  $('#load_more').on('click', searchFlickr);
+  $('#query').on('change', function () {
+    endOfImages = false;
+  });
 
   $(window).on('scroll', function () {
     if (bottomOfPage()) {
       throttledSearchFlickr();
     }
   });
-
 });
