25c8ade4f19335710191f7e7f57645450e4ac7f4
Cold Head <certainthings@gmail.com>
2015-02-25 12:45:27 +1100

    more ajax examples

--- 08-ajax/ajax-intro/main.rb
+++ 08-ajax/ajax-intro/main.rb
@@ -1,12 +1,13 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'json'
 
 get '/' do
   erb :index
 end
 
 get '/bros' do
-  %w{Gummo}.sample
+  %w{Groucho Harpo Chico Gummo}.sample
 end
 
 get '/lotto' do
@@ -16,4 +17,14 @@ end
 get '/slow' do
   sleep 10
   "<i>haha</i>" * Random.rand(1..10)
+end
+
+get '/getjson' do
+  content_type :json
+  content = {
+    :status => 'hunky dory',
+    :luckyNumber => Random.rand(1..1000),
+    :currentTime => Time.now
+  }
+  content.to_json
 end
--- 08-ajax/ajax-intro/public/js/ajax.js
+++ 08-ajax/ajax-intro/public/js/ajax.js
@@ -1,3 +1,4 @@
+/*
 var request = new XMLHttpRequest();
 request.onreadystatechange = function () {
   console.log('readyState has changed');
@@ -14,3 +15,44 @@ request.open('GET', '/slow');
 request.send(); // Asynchronous
 
 console.log('AJAX request is probably still running');
+*/
+
+$(document).ready(function () {
+  $('#lookup').on('click', function (event) {
+    event.preventDefault(); // Don't submit this to the server, Mr Browser!
+
+    var title = $('#title').val();
+
+    var processMovie = function (movie) {
+      var $poster = $('<img>').attr('src', movie.Poster);
+      $('#result').html($poster);
+    }
+
+    var url = 'http://www.omdbapi.com/';
+    $.getJSON(url, {
+      t: title
+    }).done(processMovie);
+  });
+});
+
+$(document).ready(function () {
+  $('#getJSON').on('click', function () {
+    console.log('getJSON');
+    $.getJSON('/getjson').done(function (result) {
+      console.log('request was successful', result.luckyNumber);
+      $('#getJSONresult').html(result.currentTime);
+    }).error(function () {
+      alert('something went wrong');
+    });
+  });
+});
+
+$(document).ready(function () {
+  $('#load').on('click', function () {
+    // $.get('/slow').done(function (html) {
+    //   $('#loadresult').html(html);
+    // });
+    $('#loadresult').load('/slow');
+  });
+});
+
