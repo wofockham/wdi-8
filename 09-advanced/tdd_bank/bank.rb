f0d4d2009272484cdb461dcbdc2e5c13e61ac0e3
Cold Head <certainthings@gmail.com>
2015-03-09 12:01:38 +1100

    contraception

--- 09-advanced/tdd_bank/bank.rb
+++ 09-advanced/tdd_bank/bank.rb
@@ -1,7 +1,25 @@
 class Bank
   attr_reader :name
+  attr_accessor :accounts
 
   def initialize(name)
     @name = name
+    @accounts = {}
+  end
+
+  def create_account(account_name, deposit)
+    @accounts[account_name] = deposit
+  end
+
+  def deposit(account_name, amount)
+    @accounts[account_name] += amount
+  end
+
+  def balance(account_name)
+    @accounts[account_name]
+  end
+
+  def withdraw(account_name, amount)
+    @accounts[account_name] -= amount
   end
 end
--- 09-advanced/tdd_bank/spec/bank_spec.rb
+++ 09-advanced/tdd_bank/spec/bank_spec.rb
@@ -2,23 +2,60 @@ require_relative 'spec_helper.rb'
 require_relative '../bank.rb'
 
 describe Bank do
+
+  let(:bank) { Bank.new('TDD Bank') }
+
   describe '.new' do
     it 'creates a new Bank object' do
-      bank = Bank.new('TDD Bank')
       expect(bank).to_not eq nil
     end
 
     it 'should have a name' do
-      bank = Bank.new('TDD Bank')
       expect(bank.name).to eq 'TDD Bank'
+    end
+
+    it 'has no accounts' do
+      expect(bank.accounts.count).to eq 0
     end
   end
 
   describe '#create_account' do
     it 'creates an account' do
-      bank = Bank.new('TDD Bank')
       bank.create_account('Bob', 200)
       expect(bank.accounts['Bob']).to eq 200
     end
   end
+
+  describe '#deposit' do
+    it 'deposits an amount from a client into their account' do
+      bank.create_account('Bob', 200)
+      bank.deposit('Bob', 300)
+      expect(bank.accounts['Bob']).to eq(200 + 300)
+    end
+  end
+
+  describe '#balance' do
+    it 'returns the balance for client' do
+      bank.create_account('Bob', 200)
+      expect(bank.balance('Bob')).to eq 200
+    end
+  end
+
+  describe '#withdraw' do # Not an effective form of contraception
+    it 'subtracts money from the account' do
+      bank.create_account('Bob', 200)
+      bank.withdraw('Bob', 50)
+      expect(bank.balance('Bob')).to eq(200 - 50)
+    end
+
+    it 'ignores requests for withdrawals that exceed the balance' do
+      bank.create_account('Bob', 200)
+      bank.withdraw('Bob', 1_000_000)
+      expect(bank.balance('Bob')).to eq 200
+    end
+  end
 end
+
+
+
+
