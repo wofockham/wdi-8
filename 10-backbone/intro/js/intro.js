596f06193128ac441e417b05e268605fe8638955
Cold Head <certainthings@gmail.com>
2015-03-10 12:39:55 +1100

    nosebleed

--- 10-backbone/intro/js/intro.js
+++ 10-backbone/intro/js/intro.js
@@ -15,3 +15,68 @@ var Animal = Backbone.Model.extend({
   }
 });
 
+var Zoo = Backbone.Collection.extend({
+  model: Animal
+});
+
+var animal1 = new Animal({type: 'frog', ecosystem: 'pond', stripes: 30});
+var animal2 = new Animal({type: 'dog', ecosystem: 'house', stripes: 4});
+var animal3 = new Animal({type: 'bat', ecosystem: 'cave'});
+
+var gaZoo = new Zoo([animal1, animal2, animal3]);
+
+
+var ZooView = Backbone.View.extend({
+  el: '#main',
+  initialize: function () {
+    console.log('ZooView init');
+  },
+  events: {
+    'click p': 'animalClick'
+  },
+  render: function () {
+    var view = this;
+    var animalTemplate = $('#animal-template').html();
+    var animalHTML = _.template(animalTemplate);
+    this.collection.each(function (animal) {
+      view.$el.append( animalHTML(animal.toJSON()) );
+    });
+  },
+  animalClick: function () {
+    console.log('you clicked on an animal');
+  }
+});
+
+var AppRouter = Backbone.Router.extend({
+  routes: {
+    '': 'index',
+    'animals/:id': 'viewAnimal'
+  },
+  index: function () {
+    console.log('you found the home page');
+
+    var zooView = new ZooView({collection: gaZoo});
+    zooView.render();
+  },
+  viewAnimal: function () {
+    $('#main').empty();
+    console.log('you are viewing an animal');
+  }
+});
+
+$(document).ready(function () {
+  var router = new AppRouter();
+  Backbone.history.start();
+});
+
+
+
+
+
+
+
+
+
+
+
+
