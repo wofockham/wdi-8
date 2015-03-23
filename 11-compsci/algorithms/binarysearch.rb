a4ae52133cf478b94c4252485a32e77a63b224ef
Cold Head <certainthings@gmail.com>
2015-03-23 14:32:05 +1100

    binary search works

--- 11-compsci/algorithms/binarysearch.rb
+++ 11-compsci/algorithms/binarysearch.rb
@@ -1,32 +1,30 @@
-a = (1..100).map { Random.rand(1000) }.sort
-
 # Returns the index of the needle in elements OR nil
 def binary_search(elements, needle)
   min = 0
   max = elements.length - 1
-  target = nil
+  candidate = nil
 
   # Run until all possibilities are exhausted...
-  until min >= max do
-    mid = min + max / 2
-    puts "min: #{min}, max: #{max}, mid: #{mid}"
+  until min > max do
+    mid = (min + max) / 2
 
-    target = elements[mid]
-    return(mid) if target == needle # Found it! Index is in mid
+    candidate = elements[mid]
 
-    if target < needle # Too low, focus on the right half
+    puts "min #{min}, mid #{mid}, max #{max}, candidate #{candidate}"
+
+    if candidate < needle # Too low, focus on the right half
       min = mid + 1
-    elsif target > needle # Too high, focus on the left half
+    elsif candidate > needle # Too high, focus on the left half
       max = mid - 1
+    else
+      return mid # Magic
     end
   end
 
   return nil
 end
 
-puts "passed!" if a.none? { |i| p "searching: #{i}"; binary_search(a, i).nil? }
 
-require 'pry'
-binding.pry
+# a = (0..100).map { Random.rand(500) }.sort
 
-p binary_search(a, 200)
+# p binary_search(a, a.sample)
