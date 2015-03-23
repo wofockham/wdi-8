265f296d722c07f629d1892e161743be9a5ac29d
Cold Head <certainthings@gmail.com>
2015-03-23 12:30:44 +1100

    bubble sort bugfix

--- 11-compsci/algorithms/bubblesort.rb
+++ 11-compsci/algorithms/bubblesort.rb
@@ -2,9 +2,9 @@ def bubble_sort(elements)
   n = elements.length - 2 # The maximum index we'll initially consider.
 
   # Iterate repeatedly, ignoring one more element at the end each time, i.e.:
-  # (0..9)
-  # (0..8)
-  # (0..7) ...
+  # (1..9)
+  # (1..8)
+  # (1..7) ...
   n.downto(1) do |terminator|
 
     # Swap any unsorted elements between 0 and the current "end".
