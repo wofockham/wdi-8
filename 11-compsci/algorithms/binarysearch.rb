ca7c680262568f2343d09ec698931ea72e7664e2
Cold Head <certainthings@gmail.com>
2015-03-23 14:04:31 +1100

    buggy as shit

--- 11-compsci/algorithms/binarysearch.rb
+++ 11-compsci/algorithms/binarysearch.rb
@@ -7,22 +7,24 @@ def binary_search(elements, needle)
   target = nil
 
   # Run until all possibilities are exhausted...
-  until (max - min) < 2 do
-    mid = (min + max) / 2
+  until min >= max do
+    mid = min + max / 2
     puts "min: #{min}, max: #{max}, mid: #{mid}"
 
     target = elements[mid]
     return(mid) if target == needle # Found it! Index is in mid
 
     if target < needle # Too low, focus on the right half
-      min = mid
+      min = mid + 1
     elsif target > needle # Too high, focus on the left half
-      max = mid
+      max = mid - 1
     end
   end
 
   return nil
 end
+
+puts "passed!" if a.none? { |i| p "searching: #{i}"; binary_search(a, i).nil? }
 
 require 'pry'
 binding.pry
